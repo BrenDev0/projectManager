403fea223b1a5bfef59f9047250ab9778e469dd2
Brendan Lahey <lahey1991@gmail.com>
2024-05-26 18:02:46 -0500

    adding tables to database

--- models.py
+++ models.py
@@ -8,6 +8,7 @@ class Database:
 
     def create_table(self):
         self.cur.execute("""CREATE TABLE IF NOT EXIST projects (
+        projectid INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         date DATE,
         language TEXT,
@@ -21,4 +22,25 @@ class Database:
     def delete(self, name):
         self.cur.execute("""DELETE FROM projects WHERE name = ?""", name)   
         self.conn.commit() 
+
+
+class Project:
+    def __init__(self):
+        self.conn = sqlite3.connect("project_manager.db")
+        self.cur = self.conn.cursor() 
+        self.create_table()  
+
+    def create_table(self, id):
+        self.cur.execute("""CREATE TABLE IF NOT EXIST ? (
+        type TEXT,
+        description, TEXT,
+        phase TEXT,
+        FOREIGNKEY(project) REFERENCES(projectid)   
+                      
+        )""", id)  
+
+    def insert(self, name, data):
+        self.cur.execute("""INSERT OR IGNORE INTO ?  VALUES (?,?,?,?)""", (name, data))       
+
+
 
